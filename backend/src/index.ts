763089b5f98248c93744ecf3009d72226117bb61
Floha <floha2011@hotmail.de>
2022-04-25 23:56:51 +0200

    fix issues cause bad tutorial

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,23 +1,14 @@
-import dotenv from 'dotenv';
-import express from 'express';
+const dotenv = require('dotenv');
+const express = require('express');
 
 // load the environment variables from the .env file
 dotenv.config({
     path: '.env'
 });
 
-/**
- * Express server application class.
- * @description Will later contain the routing system.
- */
-class Server {
-    public app = express();
-}
-
-// initialize server app
-const server = new Server();
+const app = express();
 
 // make server listen on some port
 ((port = process.env.APP_PORT || 5000) => {
-    server.app.listen(port, () => console.log(`Listening on port ${port}`));
+    app.listen(port, () => console.log(`Listening on port ${port}`));
 })();
